c27622d07e1dec7a1d3984e09d3bfb7f34a56a1c
abrahamrotschild <abrahamrotschild13@gmail.com>
2024-01-24 19:31:35 +0000

    Updating mighty

---
+++ 24012024_193135_mighty_t4usp.sh
@@ -0,0 +1,1 @@
+Only two people signed the Declaration of Independence on July 4th, JohnHancock and Charles Thomson. Most of the rest signed on August 2, butthe last signature wasn't added until 5 years later.
